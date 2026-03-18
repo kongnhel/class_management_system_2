8d82b096fd4422f7209bd8fcc1f04ee2dba01bce
KongNhel <kabkong94@gmail.com>
2026-03-18 08:32:50 +0700

    Add Alpine persist and collapse plugins

--- resources/js/app.js
+++ resources/js/app.js
@@ -5,14 +5,29 @@
 // window.Alpine = Alpine;
 
 // Alpine.start();
-import './bootstrap';
-import Alpine from 'alpinejs';
-import persist from '@alpinejs/persist';
-import collapse from '@alpinejs/collapse';
+// import './bootstrap';
+// import Alpine from 'alpinejs';
+// import persist from '@alpinejs/persist';
+// import collapse from '@alpinejs/collapse';
 
+// Alpine.plugin(persist);
+// Alpine.plugin(collapse);
+
+// window.Alpine = Alpine;
+
+// Alpine.start();
+
+import "./bootstrap";
+import Alpine from "alpinejs";
+import persist from "@alpinejs/persist";
+import collapse from "@alpinejs/collapse";
+
+// ចុះឈ្មោះ Plugin ធម្មតា
 Alpine.plugin(persist);
 Alpine.plugin(collapse);
 
+// កំណត់ Alpine ទៅក្នុង window object
 window.Alpine = Alpine;
 
-Alpine.start();
+// កុំប្រើ Alpine.start() បើអ្នកប្រើ Livewire v3
+// ព្រោះ Livewire នឹងហៅវាដោយខ្លួនឯង
