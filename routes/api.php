0023436a61855fd05c86c612815196f23bcd6ebc
KongNhel <kabkong94@gmail.com>
2026-04-25 19:00:48 +0700

    hfe

--- routes/api.php
+++ routes/api.php
@@ -8,3 +8,21 @@ Route::get('/user', function (Request $request) {
     return $request->user();
 })->middleware('auth:sanctum');
 
+Route::get('/debug', function () {
+    return response()->json([
+        'php_version' => PHP_VERSION,
+        'app_env' => env('APP_ENV'),
+        'app_key' => env('APP_KEY') ? 'SET' : 'NOT SET',
+        'db_host' => env('DB_HOST') ? 'SET' : 'NOT SET',
+        'db_connection' => env('DB_CONNECTION'),
+        'db_error' => null,
+        'db_status' => function() {
+            try {
+                DB::connection()->getPdo();
+                return 'Connected';
+            } catch (\Exception $e) {
+                return $e->getMessage();
+            }
+        },
+    ]);
+});
